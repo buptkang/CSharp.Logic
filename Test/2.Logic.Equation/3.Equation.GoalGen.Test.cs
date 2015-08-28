e206e91fbfdbcef88b12c0a910098cbbcfaeb067
buptkang <buptkang@gmail.com>
2015-08-28 17:14:53 -0400

    test

--- Test/2.Logic.Equation/3.Equation.GoalGen.Test.cs
+++ Test/2.Logic.Equation/3.Equation.GoalGen.Test.cs
@@ -53,7 +53,7 @@ namespace CSharpLogic
             Assert.NotNull(goal);
             Assert.True(goal.Rhs.Equals(5));
 
-            //Test it. from Geometry.Logic
+            //Test it. from Geometry.Logic add comma
         }
 
         [Test]
